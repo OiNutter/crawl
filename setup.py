acc27022e793afc1f9c3a389ee51871a034e024e
OiNutter <willmckenzie@oinutter.co.uk>
2013-01-14 13:34:06 +0000

    Bumped version to 0.4.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if os.path.exists('README.txt'):
 
 
 setup(name='Crawl',
-	  version='0.4',
+	  version='0.4.1',
 	  url='https://github.com/OiNutter/crawl',
 	  download_url='https://nodeload.github.com/OiNutter/crawl/legacy.tar.gz/master',
 	  description='Python tool for finding files in a set of paths. Based on the Hike ruby gem',
